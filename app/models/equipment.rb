72fa97ebdf8f5b97670a7b5516051d33470225e7
Ludovic Lhuillier <lhuillier.ludovic@yahoo.com>
2019-11-26 11:39:03 +0100

    problème avec le merge

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -2,7 +2,13 @@ class Equipment < ApplicationRecord
 
   CATEGORY = ['Ski', 'Chaussures de ski', 'Batons de ski', 'Snowboard', 'Boots']
 
-  belongs_to :owner
+  belongs_to :owner, class_name: 'User'
   has_many :bookings
 
+  validates :title, presence: true
+  validates :details, presence: true, length: { minimum: 10 }
+  validates :material_category, presence: true, inclusion: { in: CATEGORY }
+  validates :location, presence: true
+  validates :picture, presence: true
+  validates :price_per_day, presence: true
 end
