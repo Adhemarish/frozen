428fd48e6e796dfde1f2b93fdc7b0646c70ce5ef
Faten Salmi <faten.salmi@gmail.com>
2019-11-26 17:21:37 +0100

    Owner-list améliorée

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -9,11 +9,4 @@ class EquipmentsController < ApplicationController
   def create
   end
 
-  def show
-    @equipment = Equipment.find(params[:id])
-  end
-
-  def equipment_params
-    params.require(:equipment).permit(:title, :details, :material_category,:location, :picture, :price_)
-  end
 end
