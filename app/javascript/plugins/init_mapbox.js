8509fc94e9d5ee417bf71b8c722eb2a5344a431f
Caroline Douet <caro.do@wanadoo.fr>
2019-11-27 15:41:56 +0100

    maps home & show

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -31,7 +31,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/caroline44300/ck3hbx79r08i51cqxrvnmpb8w'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
 
