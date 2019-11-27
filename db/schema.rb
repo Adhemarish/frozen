a3c14db4f650abb5a6731b61c38281b8eebfe872
Caroline Douet <caro.do@wanadoo.fr>
2019-11-27 11:41:29 +0100

    Updated owner bookings page + default satus booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_155900) do
+ActiveRecord::Schema.define(version: 2019_11_27_102338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_155900) do
     t.date "starting_date"
     t.date "ending_date"
     t.integer "total_price"
-    t.string "status", default: "Pending"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["equipment_id"], name: "index_bookings_on_equipment_id"
@@ -43,8 +43,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_155900) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "
-    ", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "name"
     t.string "avatar"
     t.string "reset_password_token"
