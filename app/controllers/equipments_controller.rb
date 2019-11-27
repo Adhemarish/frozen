2e09e96103f68f7583fffd29f73b5b1c0b1f9b9b
Caroline Douet <caro.do@wanadoo.fr>
2019-11-27 14:23:16 +0100

    map for homepage

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -2,7 +2,16 @@ class EquipmentsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @equipments = Equipment.all
+    @equipments = Equipment.geocoded #returns equipments with coordinates
+
+    @markers = @equipments.map do |equipment|
+      {
+        lat: equipment.latitude,
+        lng: equipment.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { equipment: equipment }),
+        image_url: helpers.asset_url('location.svg')
+      }
+    end
   end
 
   def new
