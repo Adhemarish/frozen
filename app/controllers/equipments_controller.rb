0ad24b99a07dc1294e4c4bd7aae1c7cb0e96085b
Neven DREAN <nevendrean@yahoo.fr>
2019-11-28 11:35:12 +0100

    calcul js & search

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,10 +1,46 @@
 class EquipmentsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :search]
 
   def index
-    @equipments = Equipment.where.not(owner: current_user) #returns equipments with coordinates
+    @list_cities = Equipment.all.map { |equipment| equipment.location }.uniq
+    @equipments = Equipment.where.not(owner: current_user)
+    #returns equipments with coordinates
 
-    @markers = @equipments.map do |equipment|
+    #@markers = do_mark(@equipments)
+    # @markers = @equipments.map do |equipment|
+    #   {
+    #     lat: equipment.latitude,
+    #     lng: equipment.longitude,
+    #     infoWindow: render_to_string(partial: "info_window", locals: { equipment: equipment }),
+    #     image_url: helpers.asset_url('location.svg')
+    #   }
+    # end
+  end
+
+  def search
+    @list_cities = Equipment.all.map { |equipment| equipment.location }.uniq
+    #@markers = do_mark(@equipments)
+    if params[:query].present?
+      @equipments = Equipment.where(material_category: params[:query])
+    else
+      @equipments = Equipment.all
+    end
+  end
+
+  def show
+    @equipment = Equipment.find(params[:id])
+    @booking = Booking.new
+    @markers = [{
+        lat: @equipment.latitude,
+        lng: @equipment.longitude,
+        image_url: helpers.asset_url('location.svg')
+    }]
+  end
+
+  private
+
+  def do_mark(equipments)
+    equipments.map do |equipment|
       {
         lat: equipment.latitude,
         lng: equipment.longitude,
@@ -12,23 +48,6 @@ class EquipmentsController < ApplicationController
         image_url: helpers.asset_url('location.svg')
       }
     end
-
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  def show
-    @equipment = Equipment.find(params[:id])
-    @booking = Booking.new
-
-    @markers = [{
-        lat: @equipment.latitude,
-        lng: @equipment.longitude,
-        image_url: helpers.asset_url('location.svg')
-    }]
-  end
 end
