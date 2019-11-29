7cca2ecd6a39d4917070d607d3632b3d534b8c6d
Ludovic Lhuillier <lhuillier.ludovic@yahoo.com>
2019-11-29 14:43:30 +0100

    neven r&d

--- app/javascript/components/totalPrice.js
+++ app/javascript/components/totalPrice.js
@@ -1,26 +1,21 @@
-// const pricePerDay = parseInt(document.getElementById('price_per_day').innerHTML)
-// const starting = document.getElementById('booking_starting_date')
-// const ending =document.getElementById('booking_ending_date')
-// console.log(pricePerDay)
-
-// let days = 0
-
-// const calcDays = () => {
-//   const nbJours = parseInt(new Date(ending.value) - new Date(starting.value))/86400/1000 + 1
-//   return (nbJours >= 0) ?  nbJours : 0
-//  }
-
-// const calculateDays  = () => {
-//   days = calcDays();
-//   document.getElementById('total').innerHTML  = pricePerDay * days + " €"
-// }
-
-
-// starting.addEventListener('change', (event) => {
-//   calculateDays()
-//  })
-
-// ending.addEventListener('change', (event) => {
-//   calculateDays()
-//  })
-
+const priceDay = document.getElementById('price_per_day')
+if (priceDay) {
+  const pricePerDay = parseInt(priceDay.innerHTML)
+  const starting = document.getElementById('booking_starting_date')
+  const ending =document.getElementById('booking_ending_date')
+  let days = 0
+  const calcDays = () => {
+    const nbJours = parseInt(new Date(ending.value) - new Date(starting.value))/86400/1000 + 1
+    return (nbJours >= 0) ?  nbJours : 0
+   }
+  const calculateDays  = () => {
+    days = calcDays();
+    document.getElementById('total').value  = "Soumettre votre réservation pour "+ pricePerDay * days + " €"
+  }
+  starting.addEventListener('change', (event) => {
+    calculateDays()
+   })
+  ending.addEventListener('change', (event) => {
+    calculateDays()
+   })
+}
